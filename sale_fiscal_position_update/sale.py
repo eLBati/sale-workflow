b36687a2e696f772683336df8834d861438bfaaa
eLBati <lorenzo.battistini@agilebg.com>
2016-09-30 12:17:44 +0200

    IMP metadata

--- sale_fiscal_position_update/sale.py
+++ sale_fiscal_position_update/sale.py
@@ -1,25 +1,13 @@
 # -*- coding: utf-8 -*-
 #
-#
 #    Sale Fiscal Position Update module for OpenERP
 #    Copyright (C) 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
 #    Copyright (C) 2014 Akretion (http://www.akretion.com)
 #    @author Mathieu Vatel <mathieu _at_ julius.fr>
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#    Copyright 2016 Lorenzo Battistini - Agile Business Group
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 #
 
 from openerp.osv import orm
